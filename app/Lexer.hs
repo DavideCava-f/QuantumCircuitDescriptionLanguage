0bb4ac3720a9880d21e8a18181b856e96ee08181
DavideCava-f <davide.cavallo2@studio.unibo.it>
2026-03-24 19:01:52 +0100

    NoTyped Parser funziona(sembra)

--- app/Lexer.hs
+++ app/Lexer.hs
@@ -6,28 +6,28 @@ import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 
--- Definiamo il tipo base del nostro Parser
 type Parser = Parsec Void String
 
--- Space Consumer: ignora spazi e commenti stile C (// e /* */)
+-- Space Consumer (// e /* */)
 sc :: Parser ()
 sc = L.space space1 (L.skipLineComment "//") (L.skipBlockComment "/*" "*/") 
 
 rWord :: String -> Parser String
 rWord w = (lexeme . try) (string w <* notFollowedBy alphaNumChar)
 
--- Wrapper per i token: ogni atomo consuma lo spazio che lo segue
+
+
+-- Wrapper definisco wrapper per lessemi e simboli
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme sc
 
--- Wrapper per stringhe fisse (simboli)
 symbol :: String -> Parser String
 symbol = L.symbol sc
 
 reservedWords :: [String]
 reservedWords = ["let", "in", "if", "then", "else", "bit", "qbit"]
 
--- Parser per identificatori (nomi di variabili o funzioni)
+-- Parser per identificatori 
 identifier :: Parser String
 identifier = (lexeme . try) (p >>= check)
   where
@@ -36,22 +36,22 @@ identifier = (lexeme . try) (p >>= check)
               then fail $ "La parola riservata '" ++ x ++ "' non può essere un nome"
               else return x
  
- -- Parentesi e delimitatori
+ -- Parentesi
 parens :: Parser a -> Parser a
 parens = between (symbol "(") (symbol ")")
 
 angles :: Parser a -> Parser a
 angles = between (symbol "<") (symbol ">")
 
--- Operatori e punteggiatura
+-- Utils per il parsing
 arrow  :: Parser String
 arrow  = symbol "->"
 
 tensor :: Parser String
-tensor = symbol "⊗" <|> symbol "*" -- Supportiamo entrambi
+tensor = symbol "⊗" <|> symbol "*" 
 
 lambda :: Parser String
-lambda = symbol "λ" <|> symbol "\\" -- Carattere corrisponde a \
+lambda = symbol "λ" <|> symbol "\\" 
 
 dot, colon, equal, comma :: Parser String
 dot    = symbol "."
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Text.Megaparsec -- Qui c'è la funzione 'parse' corretta
+import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 import Data.Void
@@ -11,92 +11,164 @@ type Name = String
 data Type = TBit | TQbit | TFun Type Type | TPair Type Type
   deriving (Eq, Show)
 
+
 data Term 
-  = App Term Term
-  | Let Name Term Term
+  = App Term Term -- H(x) CNOT(x,y) 
+  | Let Name Term Term -- let x = q1 in H(x)
   | Decomp Name Name Term Term  -- let <x,y> = t in t
-  | If Term Term Term
+  | If Value Term Term
   | Gate String [Term]          -- Per U(v1...vn) come H, X, CNOT
+  | V Value
   deriving (Show)
 
 
 data Value
   = Var Name
-  | Lambda Name Type Term
+  | Lambda Name Term
   | Pair Term Term
   deriving (Show)
+-- Parsing Dei tipi
+pTypeAtom :: Parser Type
+pTypeAtom = 
+      (rWord "bit"  >> return TBit)
+  <|> (rWord "qbit" >> return TQbit)
+  <|> parens pType  
 
-{-
----Parsing Atomi
-intParser :: Parser N
-intParser = do
-    spaces
-    ds <- (many1 digit)
-    spaces
-    --notFollowedBy alphaNum 
-    spaces
-    return (LitInt (read ds))
+pType :: Parser Type
+pType = do
+  t1 <- pTypeAtom
+  (do 
+      symbol "->" 
+      t2 <- pType 
+      return (TFun t1 t2)
+   <|> do 
+      symbol "⊗" -- o "*" 
+      t2 <- pType 
+      return (TPair t1 t2)
+   <|> return t1) 
 
-varParser :: Parser N
-varParser = do
-    spaces
-    v <- many1 letter
-    spaces
-    --notFollowedBy alphaNum
-    return (Var v)
+--------
+---- Term Parser
 
-atomParser :: Parser N
-atomParser = try intParser <|> varParser
+termParser :: Parser Term
+termParser = try letParser <|> try decompParser <|> try ifParser <|> try gateParser <|> applicationParser
 
-
-
-parensParser :: Parser N
-parensParser = do
-  spaces
-  char '('
-  spaces
-  e1 <- exprParser
-  spaces
-  op <- (char '+' >> return Add) <|> (char '*' >> return Mul)
-  spaces
-  e2 <- exprParser
-  spaces
-  char ')'
-  spaces
-  return (op e1 e2)
-
-letParser :: Parser N
+letParser :: Parser Term
 letParser = do
-    spaces
-    char '('
-    spaces
-    string "let"
-    spaces
-    v <- many1 letter
-    spaces
-    char '='
-    spaces
-    e1 <- exprParser 
-    spaces
-    string "in"
-    spaces
-    e2 <- exprParser
-    spaces
-    char ')'
-    spaces
-    return(Let v e1 e2)
+    rWord "let"
+    v <- identifier
+    equal 
+    t1 <- termParser  
+    rWord "in"
+    t2 <- termParser
+    return(Let v t1 t2)
+
+decompParser :: Parser Term
+decompParser = do
+    rWord "let"
+    (i1, i2) <- angles $ do
+        v1 <- identifier
+        comma
+        v2 <- identifier
+        return (v1, v2)
+    equal
+    t1 <- termParser
+    rWord "in"
+    t2 <- termParser
+    return(Decomp i1 i2 t1 t2)
+
+ifParser :: Parser Term
+ifParser = do
+    rWord "if"
+    v <- valueParser
+    rWord "then"
+    t1 <- termParser
+    rWord "else"
+    t2 <- termParser
+    return(If v t1 t2)
+
+validGates :: Parser String
+validGates = choice [ string "H"
+                    , string "X"
+                    , string "CNOT"
+                    , string "M"
+                    ] <* sc
+
+gateParser :: Parser Term
+gateParser = do
+    name <- identifier 
+    case name of
+        "CNOT" -> do
+            args <- parens $ do
+                t1 <- termParser
+                comma
+                t2 <- termParser
+                return [t1, t2]
+            return (Gate name args)
+            
+        "H" -> oneArgGate name
+        "X" -> oneArgGate name
+        "M" -> oneArgGate name
+        
+        _ -> fail $ "Unknown gate: " ++ name
+
+oneArgGate :: String -> Parser Term
+oneArgGate name = do
+    arg <- parens termParser 
+    return (Gate name [arg])
+
+----
+
+atomParser :: Parser Term
+atomParser = (V <$> valueParser)
+    <|> parens termParser
+
+applicationParser:: Parser Term
+applicationParser = do
+  atoms <- some atomParser      --[a1, a2, a3...]
+  return (foldl1 App atoms) -- App (App a1 a2) a3
+
+--------
+
+valueParser :: Parser Value
+valueParser = try pairParser <|> try  lambdaParser <|> varParser 
 
 
-exprParser :: Parser N
-exprParser = try letParser <|> try parensParser <|> atomParser
--}
+lambdaParser :: Parser Value
+lambdaParser = do
+    lambda
+    x <- identifier
+    dot
+    t <- termParser
+    return(Lambda x t)
+
+pairParser :: Parser Value
+pairParser = do
+    (i1, i2) <- angles $ do
+        v1 <- termParser
+        comma
+        v2 <- termParser
+        return (v1, v2)
+    return(Pair i1 i2)
+
+
+varParser :: Parser Value
+varParser = do
+    v <- identifier
+    return(Var v)
+    
+
+
+mainParser :: Parser Term
+mainParser = sc *> termParser <* eof 
 
 
 main :: IO ()
 main = do
-    putStrLn "Inserisci l'espressione try :((l*4) + (let x = 3 in (x\t+2)))"
+    let try = "let sup = \\f.\\x. let y = H(x) in let z = f y in H(z) in sup q" 
+    putStrLn("Prova:" ++ try)
     input <- getLine
-    case Text.Megaparsec.runParser lexerTest "" input of
-        Left err  -> putStrLn (errorBundlePretty err) -- Megaparsec usa errorBundlePretty, non show
+    case Text.Megaparsec.runParser mainParser "" input of
+        Left err  -> putStrLn (errorBundlePretty err) 
         Right res -> print res
 
